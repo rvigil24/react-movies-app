0e1d33430fe0d4758f93435bd0f3a0b137d54280
Ruben Vigil <ruben.vigil24@gmail.com>
2021-07-09 21:44:03 +0100

    created global styles for app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 
+//styles
+import { GlobalStyle } from "./GlobalStyle";
+
 function App() {
-  return <div className="App">react movies app</div>;
+  return <div className="App">react movies app SS <GlobalStyle /></div>
 }
 
 export default App;
---
+++ src/GlobalStyle.js
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root{
+        --maxWidth: 1280px;
+        --white: #fff;
+        --lightGrey: #eee;
+        --medGrey: #353535;
+        --darkGrey: #1c1c1c;
+        --fontSuperBig: 2.5rem;
+        --font: 1.5rem;
+        --fontMed: 1.2rem;
+        --fontSmall: 1rem;
+    }
+    *{
+        margin:0;
+        padding:0;
+        outline:none;
+        font-family: 'Abel', sans-serif;
+        box-sizing: inherit;
+    }
+    html{
+        font-size: 62.5%;
+    }
+    body{
+        box-sizing: border-box;
+
+        h1{
+            font-size: 2rem;
+            font-weight: 600;
+            color: var(--white);
+        }
+        h3{
+            font-size: 1.1rem;
+            font-weight: 600;
+        }
+        p{
+            font-size: 1rem;
+            color: var(--white);
+        }
+    }
+`;
